75ce5a036b6467226d41545150cbaba42857265d
NixDevs <aniklakhany00@gmail.com>
2022-11-09 18:48:50 +0530

    Add enums and event class

---
+++ src/struct/Event.ts
@@ -0,0 +1,25 @@
+import type { ClientEvents } from 'discord.js';
+import Kottu from 'struct/Kottu';
+
+/**
+ * Generic Event class which provides the structure for all events.
+ *
+ * @typeParam K - Key which must be one of the following event types: {@link https://discord.js.org/#/docs/discord.js/main/typedef/Events}
+ */
+export default class Event<K extends keyof ClientEvents> {
+    public constructor(
+    /** The event type */
+    public event: K,
+
+    /**
+     * Handles all logic relating to event execution.
+     *
+     * @param client - The client to bind to the event
+     * @param args - List of arguments for the event
+     */
+    public run: (
+      kottu: Kottu,
+      ...args: ClientEvents[K]
+    ) => Promise<void> | void,
+    ) {}
+}
---
+++ src/struct/enums.ts
@@ -0,0 +1,17 @@
+export enum CommandType {
+    Info = 'info',
+    Fun = 'fun',
+    Misc = 'misc'
+}
+export enum ErrorType {
+    Missing = 'Missing Permissions',
+    Failure = 'Command Failure',
+}
+export enum Color {
+    Red = '#FF0000',
+    White = '#FFFFFF',
+    Black = '#000001',
+    Blue = '#2b2bff',
+    Purple = '#a107fa',
+    Cyan = '#00bbff'
+}
