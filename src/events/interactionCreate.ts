b609e750cf7094804d0b6817ea694346cee9c027
NixDevs <nixdevs@ALakhany.localdomain>
2022-11-13 07:54:36 +0530

    style(interactionCreate): testing inclusive of nullish coalescing for beautify
    Probs switch to prettierc

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -1,14 +1,17 @@
-import { Events, Interaction } from 'discord.js';
-import  Event  from 'struct/Event';
+import {
+    Events,
+    Interaction
+} from 'discord.js';
+import Event from 'struct/Event';
 import Kottu from 'struct/Kottu';
 
-export default new Event(Events.InteractionCreate, async (kottu: Kottu, interaction: Interaction): Promise<void> => {
+export default new Event(Events.InteractionCreate, async (kottu: Kottu, interaction: Interaction): Promise < void > => {
     if (interaction.isChatInputCommand()) {
         console.log('a');
         const command = kottu.commands.get(interaction.commandName);
         console.log(command);
         if (!command) {
-            await interaction.reply('This command is unavailable!'); 
+            await interaction.reply('This command is unavailable!');
             Promise.resolve();
         }
         command?.execute(interaction);
