36064046a729bb24ae8a1c8217024155f044c916
NixDevs <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>
2023-01-04 19:28:00 +0530

    fix(enums): more spell errors

--- src/enums.ts
+++ src/enums.ts
@@ -36,5 +36,5 @@ export enum ButtonCustomIds {
     CAH_ENTRY = 'CAH Button',
     CAH_SUBMIT_MENU = 'CAH Submit Menu',
     CAH_SUBMIT_BUTTON = 'CAH submit Button',
-    CA_VOTE_BUTTON = 'CAH vote button',
+    CAH_VOTE_BUTTON = 'CAH vote button',
 }
